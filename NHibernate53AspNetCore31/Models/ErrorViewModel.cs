656af3cd904d8051bf9cbc279d08297c55170405
Raphael Basso <arabasso@yahoo.com.br>
2020-07-28 14:53:20 -0300

    Adicionar arquivos de projeto.

---
+++ NHibernate53AspNetCore31/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NHibernate53AspNetCore31.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
